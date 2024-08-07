6a85a04e747ede49cb67804572b70ab01308ba11
Kicamon <kicamonice@gmail.com>
2024-08-07 21:59:52 +0800

    use async

--- lua/markdown-table-mode/init.lua
+++ lua/markdown-table-mode/init.lua
@@ -1,3 +1,5 @@
+local mtm_group = vim.api.nvim_create_augroup('mtm', {})
+
 ---Check if the line_number is a markdown table
 ---@param line_number integer
 ---@return integer
@@ -72,7 +74,7 @@ local function update_cell_contents(table_contents, width)
   end
 
   local function get_table_line_char_id(chars) -- get chars at second line's left and right
-    for i, v in ipairs(table_line_char) do -- leave insert
+    for i, v in ipairs(table_line_char) do     -- leave insert
       if chars[1] == v[1] and chars[2] == v[2] then
         return i
       end
@@ -124,52 +126,51 @@ local function cells_to_table(table_contents)
   return table_contents
 end
 
-local function format_markdown_table()
-  if not check_markdown_table(vim.fn.line('.')) then -- check if the curso is in markdown table
-    return
-  end
-
-  -- find the staring line and ending line of markdown table
-  local table_start_line, table_end_line = find_markdown_table(-1), find_markdown_table(1)
-  local table_contents = {}
-
-  ---change table to cells
-  ---@param line string
-  ---@param lnum integer
-  local function table_to_cells(line, lnum)
-    local table_cells = {}
-    for cell in line:gmatch("([^|]+)%|") do
-      if lnum ~= 1 then
-        cell = cell:match("^%s*(.-)%s*$")
-      end
-      table.insert(table_cells, cell)
+local fmt = coroutine.create(function()
+  while true do
+    if not check_markdown_table(vim.fn.line('.')) then -- check if the curso is in markdown table
+      return
     end
-    table.insert(table_contents, table_cells)
+
+    -- find the staring line and ending line of markdown table
+    local table_start_line, table_end_line = find_markdown_table(-1), find_markdown_table(1)
+    local table_contents = {}
+
+    ---change table to cells
+    ---@param line string
+    ---@param lnum integer
+    local function table_to_cells(line, lnum)
+      local table_cells = {}
+      for cell in line:gmatch("([^|]+)%|") do
+        if lnum ~= 1 then
+          cell = cell:match("^%s*(.-)%s*$")
+        end
+        table.insert(table_cells, cell)
+      end
+      table.insert(table_contents, table_cells)
+    end
+
+    -- traversal markdown table lines
+    for lnum = table_start_line, table_end_line, 1 do
+      local line = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, true)[1]
+      table_to_cells(line, lnum - table_start_line)
+    end
+
+    local width = get_markdown_table_cells_width(table_contents)
+
+    table_contents = update_cell_contents(table_contents, width)
+    table_contents = cells_to_table(table_contents)
+
+    vim.api.nvim_buf_set_lines(0, table_start_line - 1, table_end_line, true, table_contents)
+    coroutine.yield()
   end
+end)
 
-  -- traversal markdown table lines
-  for lnum = table_start_line, table_end_line, 1 do
-    local line = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, true)[1]
-    table_to_cells(line, lnum - table_start_line)
-  end
-
-  local width = get_markdown_table_cells_width(table_contents)
-
-  table_contents = update_cell_contents(table_contents, width)
-  table_contents = cells_to_table(table_contents)
-
-  vim.api.nvim_buf_set_lines(0, table_start_line - 1, table_end_line, true, table_contents)
+local function format_markdown_table()
+  vim.schedule(function()
+    coroutine.resume(fmt)
+  end)
 end
-
-local opt = {
-  filetype = {
-    '*.md',
-  },
-  options = {
-    insert = true, -- when typeing "|"
-    insert_leave = true, -- when leaveing insert
-  }
-}
 
 local function format_markdown_table_lines()
   local current_line = vim.api.nvim_get_current_line()
@@ -182,9 +183,20 @@ local function format_markdown_table_lines()
   end
 end
 
+local opt = {
+  filetype = {
+    '*.md',
+  },
+  options = {
+    insert = true,       -- when typeing "|"
+    insert_leave = true, -- when leaveing insert
+  }
+}
+
 local function setup(opts)
   opt = vim.tbl_extend('force', opt, opts or {})
   vim.api.nvim_create_autocmd('InsertLeave', {
+    group = mtm_group,
     pattern = opt.filetype,
     callback = function()
       if opt.options.insert_leave then
@@ -193,6 +205,7 @@ local function setup(opts)
     end
   })
   vim.api.nvim_create_autocmd('TextChangedI', {
+    group = mtm_group,
     pattern = opt.filetype,
     callback = function()
       if opt.options.insert then
